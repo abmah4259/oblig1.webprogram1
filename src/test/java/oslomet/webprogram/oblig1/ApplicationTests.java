0e393c45bf3ae8d9d50ad299adb543d33a76f1a7
abmah4259 <abmah4259@oslomet.no>
2024-04-22 13:40:33 +0200

    Initial commit

---
+++ src/test/java/oslomet/webprogram/oblig1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.webprogram.oblig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
